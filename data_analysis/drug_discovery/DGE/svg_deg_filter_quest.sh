ed5c7c237d29aec2af67bce2b0ee206039ab09c4
shaddock-123 <63892654+shaddock-123@users.noreply.github.com>
2025-05-09 21:10:59 -0500

    Update svg_deg_filter_quest.sh

--- data_analysis/drug_discovery/DGE/svg_deg_filter_quest.sh
+++ data_analysis/drug_discovery/DGE/svg_deg_filter_quest.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
-#SBATCH -A b1042
-#SBATCH -p genomics
+#SBATCH -A p31931
+#SBATCH -p short
 #SBATCH -t 1:00:00
 #SBATCH -N 1
 #SBATCH -n 1
-#SBATCH --array=0-4
+#SBATCH --array=0-10
 #SBATCH --mem=24G
-#SBATCH --mail-user=yanyi.ding@northwestern.edu
+#SBATCH --mail-user=lru4593@northwestern.edu
 #SBATCH --mail-type=END,FAIL
 #SBATCH --job-name="deg_svg_%a"
-#SBATCH --output=/projects/b1131/ydn4687/SpatialT/cosmx_colon_revision/svg/out/deg_svg_%a.out
+#SBATCH --output=/projects/b1131/SpatialT/10x/DGE/out_samples_from_reviewer2_10x_sup20250505/deg_svg_%a.out
 
 module purge all
-module load python-miniconda3/4.12.0
-source activate myenv
+conda activate drug_PPI
+export PATH="/projects/b1131/lru4593/env/drug_PPI/bin:$PATH"
+### source activate myenv
 
-IFS=$'\n' read -d '' -r -a input_args < /projects/b1131/SpatialT/cosmx_patho_samples.tsv
-IFS=$' ' read -ra split_dirs <<< ${input_args[${SLURM_ARRAY_TASK_ID}]}
-dsid=${split_dirs[0]}
-sampleid=${split_dirs[1]}
-tech=${split_dirs[2]}
+IFS=$'\n' read -d '' -r -a input_args < /projects/b1131/SpatialT/10x/DGE/samples_from_reviewer2_10x_sup20250505.txt
+IFS=$'\t' read -ra split_dirs <<< ${input_args[${SLURM_ARRAY_TASK_ID}]}
+pid=${split_dirs[0]}
+dsid=${split_dirs[1]}
+sampleid=${split_dirs[2]}
+tech=${split_dirs[3]}
 
+echo "PID: ${pid}"
 echo "DSID: ${dsid}"
 echo "SampleID: ${sampleid}"
 echo "Tech: ${tech}"
 
-python /projects/b1131/SpatialT/ST-dataset/analysis/database_utilities/SVG/svg_deg_filter_cosmx_case_quest.py $dsid $sampleid $tech
+python /projects/b1131/SpatialT/10x/DGE/svg_deg_filter_quest.py $pid $dsid $sampleid $tech
